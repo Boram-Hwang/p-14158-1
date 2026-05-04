d0223d0b843a0a9095120798a0e76379fbceb821
boramH <ky26212621@gmail.com>
2026-05-04 15:39:21 +0900

    날짜 컬럼 추가

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -3,6 +3,8 @@ package com.back.domain.post.post.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Setter
@@ -12,12 +14,16 @@ public class Post {
     @Id // PRIMARY KEY
     @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
     private int id; // INT
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
     private String title; // varchar(255)
     @Column(columnDefinition = "TEXT")
     private String content;
 
 
     public Post(String title, String content) {
+        this.createDate = LocalDateTime.now();
+        this.modifyDate = LocalDateTime.now();
         this.title = title;
         this.content = content;
     }
