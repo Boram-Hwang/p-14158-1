c7e5912b04366f41d494abab245f204bc973af80
boramH <ky26212621@gmail.com>
2026-05-04 15:32:44 +0900

    생성자 수정

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,19 +1,24 @@
 package com.back.domain.post.post.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
+@ToString
+@NoArgsConstructor
 public class Post {
     @Id // PRIMARY KEY
     @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
     private int id; // INT
     private String title; // varchar(255)
+    @Column(columnDefinition = "TEXT")
     private String content;
+
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
