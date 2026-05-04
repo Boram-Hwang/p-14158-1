ee0cb8764f38542bce9c04433b11e958184cfc98
boramH <ky26212621@gmail.com>
2026-05-04 12:42:32 +0900

    리포지토리 생성

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,4 +1,19 @@
 package com.back.domain.post.post.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Post {
+    @Id // PRIMARY KEY
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+    private int id; // INT
+    private String title; // varchar(255)
+    private String content;
 }
